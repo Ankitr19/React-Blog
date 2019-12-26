e06ff4c1726a4ffabaac3fad6b4d1e7d16e6aaa7
Ankit <rajankit510@gmail.com>
2019-12-26 09:33:02 +0530

    first commit

---
+++ src/build/precache-manifest.a7315843735ae0ab8ca5a844c6641f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95fbb2b4768a7ffeef97928327c441e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4e4170cf6e540512180",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "ad30d7a483d666c26785",
+    "url": "/static/js/2.e1b0c06d.chunk.js"
+  },
+  {
+    "revision": "d4e4170cf6e540512180",
+    "url": "/static/js/main.25f32eb9.chunk.js"
+  },
+  {
+    "revision": "a16a5b2b3d40452231a8",
+    "url": "/static/js/runtime-main.abcb2c6b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
